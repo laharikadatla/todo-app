c896e62b2644fa85718e23d4b42fbb47df700d53
laharika <datlalaharika8@gmail.com>
2021-07-07 22:16:24 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <TodoList v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/todoList';
+export default {
+  name: 'app',
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Go workout',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Do laundry',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Cook food',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Clean up room',
+          completed: false
+        },
+        {
+          i: 5,
+          title: 'Finish work',
+          completed: false
+        }
+      ],
+    }
+  },
+}
+</script>
+
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2>My todolist</h2>
+    <ul>
+      <li v-bind:key="todo.id" v-for="todo in todos">
+        <p>{{ todo.title }}</p>
+        <!-- <Todo v-bind:todo="todo" /> -->
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+// import Todo from './todo';
+export default {
+  name: 'TodoList',
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Go workout',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Do laundry',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Cook food',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Clean up room',
+          completed: false
+        },
+        {
+          i: 5,
+          title: 'Finish work',
+          completed: false
+        }
+      ],
+    }
+  },
+  // components: {
+  //   Todo
+  // },
+  // props: [
+  //   "todos"
+  // ]
+}
+</script>
+<style scoped>
+</style>
